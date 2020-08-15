dee294890a37ee17bce693fbc1c17aab5c3823db
D.Wajdi <wajdi.dahech@esprit.tn>
2020-08-15 03:59:24 -0700

    Initial Commit

---
+++ BookStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookStore
+//
+//  Created by Katsu on 8/15/20.
+//  Copyright © 2020 Katsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
